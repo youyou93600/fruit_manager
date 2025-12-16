0d0a321a4a832621c9858c0b3925bd0b34fec400
youyou93600 <younes.brahimi.medecine@gmail.com>
2025-12-16 10:04:33 +0100

    ajout de la fonctionnalité trésorerie

--- fruit-manager.py
+++ fruit-manager.py
@@ -1,15 +1,30 @@
 import json
 
 
-def ouvrir_inventaire(path="inventaire.json"):
+def ouvrir_inventaire(path="data/inventaire.json"):
     with open(path, "r", encoding="utf-8") as fichier:
         inventaire = json.load(fichier)
     return inventaire
 
 
-def ecrire_inventaire(inventaire, path="inventaire.json"):
+def ecrire_inventaire(inventaire, path="data/inventaire.json"):
     with open(path, "w", encoding="utf-8") as fichier:
         json.dump(inventaire, fichier, ensure_ascii=False, indent=4)
+
+
+def ouvrir_tresorerie(path="tresorerie.txt"):
+    with open(path, "r", encoding="utf-8") as fichier:
+        tresorerie = json.load(fichier)
+    return tresorerie
+
+
+def ecrire_tresorerie(tresorerie, path="tresorerie.txt"):
+    with open(path, "w", encoding="utf-8") as fichier:
+        json.dump(tresorerie, fichier, ensure_ascii=False, indent=2)
+
+
+def afficher_tresorerie(tresorerie):
+    print(f"la tresorerie actuelle est de {tresorerie} euros")
 
 
 def afficher_inventaire(inventaire):
@@ -25,11 +40,13 @@ def recolter(inventaire, fruit, quantité):
     print(f"nous avons recolté {quantité} {fruit} supplémentaire")
 
 
-def vendre_fruits(inventaire, fruit, quantité):
+def vendre_fruits(inventaire, fruit, quantité, tresorerie):
     if inventaire.get(fruit, 0) >= quantité:
         inventaire[fruit] -= quantité
         ecrire_inventaire(inventaire)
+        tresorerie += 1 * quantité
         print(f"Vendu: {quantité} unités de {fruit}")
+        return (inventaire, tresorerie)
     else:
         print(
             "il n'y a pas assez de quantité de {fruit} dans l'inventaire pour vendre {quantité} {fruit}"
@@ -38,7 +55,11 @@ def vendre_fruits(inventaire, fruit, quantité):
 
 if __name__ == "__main__":
     inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
     afficher_inventaire(inventaire)
+    afficher_tresorerie(tresorerie)
     recolter(inventaire, "bananes", 30)
-    vendre_fruits(inventaire, "bananes", 5)
-    afficher_inventaire(inventaire)
+    inventaire, tresorerie = vendre_fruits(inventaire, "bananes", 5, tresorerie)
+
+    ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
