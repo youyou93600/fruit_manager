536a8f4b0c08b12dd88ca6b7c06406382acc6a4a
youyou93600 <younes.brahimi.medecine@gmail.com>
2025-12-17 09:58:11 +0100

    ajout de la fonctionnalité streamlit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from fruit_manager import *
+
+st.title(" Dashboard de la Plantation")
+
+inventaire = ouvrir_inventaire()
+prix = ouvrir_prix()
+tresorerie = ouvrir_tresorerie()
+
+st.header("Trésorerie")
+st.metric(label="Montant disponible", value=f"{tresorerie:.2f} $")
+
+st.header("Inventaire")
+st.table(inventaire)
